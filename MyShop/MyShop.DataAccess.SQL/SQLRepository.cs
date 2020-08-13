3cf44861733bbfaf80c827c2ae298ba9c9c86480
ehtisham80 <ehtisham_zaheer@hotmail.com>
2020-08-13 12:59:58 +0200

    Product Filtering

--- MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -33,10 +33,9 @@ namespace MyShop.DataAccess.SQL
         {
             var t = Find(Id);
             if(context.Entry(t).State == EntityState.Detached)
-            {
+            
                 dbSet.Attach(t);
                 dbSet.Remove(t);
-            }
         }
 
         public T Find(string Id)
